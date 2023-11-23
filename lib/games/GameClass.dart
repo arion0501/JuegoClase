94d8c95a3727c663e56f9081533a91d311a25e4b
Marcos García <markarules01@gmail.com>
2023-11-23 19:36:32 +0100

    Adición mapa principal y personaje duplicado, dependencias e imagenes.

--- lib/games/GameClass.dart
+++ lib/games/GameClass.dart
@@ -1,18 +1,18 @@
 import 'dart:ui';
-
-import 'package:flame/camera.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
-
+import 'package:flame_tiled/flame_tiled.dart';
 import '../players/PlayerClass.dart';
 
 class GameClass extends FlameGame {
 
   GameClass();
 
-  late PlayerClass _ember;
+  @override
   final world = World();
   late final CameraComponent cameraComponent;
+  late PlayerClass _ember, _ember2;
+  late TiledComponent mapComponent;
 
   @override
   Color backgroundColor() {
@@ -27,6 +27,8 @@ class GameClass extends FlameGame {
       'heart.png',
       'star.png',
       'water_enemy.png',
+      'mapa1.png',
+      'mapa2.png'
     ]);
 
     cameraComponent = CameraComponent(world: world);
@@ -36,9 +38,18 @@ class GameClass extends FlameGame {
     cameraComponent.viewfinder.anchor = Anchor.topLeft;
     addAll([cameraComponent, world]);
 
+    mapComponent = await TiledComponent.load('mapa1.tmx', Vector2.all(32));
+    world.add(mapComponent);
+
     _ember = PlayerClass(
       position: Vector2(128, canvasSize.y - 70),
     );
+
+    _ember2 = PlayerClass(
+      position: Vector2(328, canvasSize.y - 70),
+    );
+
     world.add(_ember);
+    world.add(_ember2);
   }
 }
